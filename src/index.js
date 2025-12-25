ca89f8de5ff50461070fb2a6dd287924413c46d3
Hieronymus <hieronymus@git.com>
2025-12-25 16:35:12 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,74 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const BOT_TOKEN = env.BOT_TOKEN
+		if (request.method !== "POST") return new Response("OK");
+
+		let body;
+		try {
+			body = await request.json();
+		} catch (err) {
+			console.error("body is not a json.", err);
+			return new Response("Bad Request", { status: 400 });
+		}
+
+		const chatId = body.message?.chat?.id;
+		const text = body.message?.text;
+
+		console.log("body:", body);
+
+		if (chatId && text) {
+			try {
+				if (text === `/showapps`) {
+					await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							chat_id: chatId,
+							text: `Press a button to open an applet.`,
+							reply_markup: {
+								inline_keyboard: [
+									[
+										{ text: "Tool Box", web_app: { url: "https://hieronymus-toolbox.pages.dev/" } },
+										{ text: "3D Avatar", web_app: { url: "https://animation-hieronymus-3js.pages.dev/" } },
+										{ text: "Secret Post", web_app: { url: "https://secret-post-frontend-reconstructed.pages.dev/" } }
+									]
+								]
+							}
+						}),
+					});
+				} else if (text === `/help`) {
+					await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							chat_id: chatId,
+							text: `Enter /showapps to open the applet.`,
+						}),
+					});
+				} else if (text === `/start`) {
+					await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							chat_id: chatId,
+							text: `You are all set and you can now use the applet.\nYour chatID is ${chatId}.`,
+						}),
+					});
+				} else {
+					await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							chat_id: chatId,
+							text: `Unknown command.`,
+						}),
+					});
+				}
+			} catch (err) {
+				console.error("Send Message Failed.", err);
+			}
+		}
+
+		return new Response("OK");
 	},
 };
